e6705342600fc041a7d664ecc5164af531756a77
Josty Qweenq <josty.qweenq@gmail.com>
2022-02-25 17:46:34 +0700

    delete debug

--- app/config.py
+++ app/config.py
@@ -7,10 +7,8 @@ class Config:
 
 
 class Dev(Config):
-    print('This app is in dev mode')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
 
 class Prod(Config):
     pass
