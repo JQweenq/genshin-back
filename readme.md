59b2f82b14057b534139a62eb58eb8f6fb18d11f
Josty Qweenq <josty.qweenq@gmail.com>
2022-03-14 21:17:52 +0300

    added ui

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Run
+
+## windows
+
+```
+pip install -r requirements.txt
+python run.py
+```
+
+## linux
+
+```
+pip3 install -r requirements.txt
+gunicorn run:app
+```
--- run.py
+++ run.py
@@ -1,19 +1,13 @@
 from app import createApp
 from app.extensions import db
-from app.tables import User, Character, Word, Wishe
 
 
 app = createApp('prod')
 db.app = app
 try:
     db.create_all()
-except:
-    print("rrr")
-
-# if (User.query.filter(User.id == 1).first() == []) or \
-#         (Character.query.filter(Character.id == 1).first() == []) or \
-#         (Word.query.filter(Word.id == 1).first() == []) or \
-#         (Wishe.query.filter(Wishe.id == 1).first() == []):
+except e:
+    print("[Error] db", e)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='192.168.43.231')
